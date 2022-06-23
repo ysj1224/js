430e304b90cb4bfe2f9632f92952958e3fa7c69b
ysj1224 <y_sj1224@naver.com>
2022-06-23 21:36:48 +0900

    js class List toggle

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    const clickClass = "active"
-    if(h1.classList.contains(clickClass)) {
-        h1.classList.remove(clickClass);
-    } else {
-        h1.classList.add(clickClass);
-    }
+    h1.classList.toggle("active")
 }
 
 h1.addEventListener("click", handleTitleClick);
