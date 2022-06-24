9b979992e7ea9096f13027dc44915b3889263623
ysj1224 <y_sj1224@naver.com>
2022-06-24 12:53:41 +0900

    simple login using localStage

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const loginForm = document.querySelector("#login-form")
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-function handleTitleClick(){
-    h1.classList.toggle("active")
+const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username"
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
-h1.addEventListener("click", handleTitleClick);
+function paintGreetings(t){
+    greeting.innerText = `Hello ${t}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
+loginForm.addEventListener("submit", onLoginSubmit)
 
-  
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+} else {
+    paintGreetings(savedUsername); 
+}
